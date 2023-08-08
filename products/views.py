22ba64c9296c9d53b8abc73a28d13abc01a57b17
Enquil <jim.e.olesen@gmail.com>
2023-08-08 19:47:32 +0000

    created products template, product detail template and urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,111 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+from .models import Category, Brand, Product, Domain
+from django.views import View
 
-# Create your views here.
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+
+            if not query:
+                messages.error(request, 'You didnt enter any search criteria')
+                return redirect(reverse('products'))
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+class ProductDetail(View):
+    """ Detailed view for chosen product """
+
+    def get(self, request, product_sku):
+
+        product = get_object_or_404(Product, sku=product_sku)
+        category_parent = str(product.category.parent)
+        size_list = None
+
+        if product.has_sizes:
+            if category_parent == 'shoes':
+                size_list = ('35', '36', '37', '38', '39', '40',
+                             '41', '42', '43', '44', '45', '46')
+            else:
+                size_list = ('XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL')
+
+        return render(request, 'products/product_detail.html',
+                      {
+                        'product': product,
+                        'category_parent': category_parent,
+                        'size_list': size_list,
+                      })
+
+
+def product_detail(request, product_id):
+    """ Detailed view for chosen product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    # Check if product has sizes
+    if product.has_sizes:
+        # If shoes
+        if product.category.name == 'shoes':
+            size_list = (
+                '35', '36', '37', '38', '39', '40',
+                '41', '42', '43', '44', '45', '46'
+            )
+        # If not shoes
+        else:
+            size_list = ('XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL')
+
+    context = {
+        'product': product,
+        'size_list': size_list,
+    }
+
+    return render(request, 'products/product_detail.html', context)
