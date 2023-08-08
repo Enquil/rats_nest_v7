a7c030abc506b7382383a48f92f3cd77e80c4f9e
Enquil <jim.e.olesen@gmail.com>
2023-08-08 20:24:27 +0000

    moved models to product app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from .models import Product, Category, Domain, Brand
 
-# Register your models here.
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'name'
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_filter = ('name',)
+
+    list_display = (
+        'id',
+        'name',
+        'domain',
+    )
+
+    search_fields = (
+        'domain',
+        'name',
+    )
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+
+    list_filter = ('name',)
+
+    list_display = (
+        'id',
+        'name'
+    )
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_filter = ('sku',)
+
+    list_display = (
+        'id',
+        'name',
+        'category',
+        'brand',
+    )
+
+    search_fields = (
+        'name',
+    )
