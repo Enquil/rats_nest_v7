b182f120fad5f39113b5122b7653362878166ebe
Enquil <jim.e.olesen@gmail.com>
2023-08-09 08:20:46 +0000

    replaced multiple categories filter with a single search param

--- products/views.py
+++ products/views.py
@@ -35,9 +35,7 @@ def all_products(request):
             products = products.order_by(sortkey)
 
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+            products = products.filter(category=request.GET['category'])
 
         if 'q' in request.GET:
             query = request.GET['q']
