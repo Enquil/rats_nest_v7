09d25d6bf69a7152879bd2fe44117bec3be74602
Enquil <jim.e.olesen@gmail.com>
2023-08-12 13:29:53 +0000

    created view for admin to add products

--- admin_actions/views.py
+++ admin_actions/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
