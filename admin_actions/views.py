131558f32bd237bca4b1c3cbe9405237f548d84e
Enquil <jim.e.olesen@gmail.com>
2023-08-12 16:44:46 +0000

    added post handling for add_product admin view

--- admin_actions/views.py
+++ admin_actions/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from .forms import ProductForm
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
-    template = 'admin_actions/admin_actions.html'
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
     context = {
         'form': form,
     }
