074ead84e78e1950b81c4cb5b498d584262d05b5
Enquil <jim.e.olesen@gmail.com>
2023-08-16 15:45:47 +0000

    created basic newsletter model, no migration

--- admin_actions/models.py
+++ admin_actions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Newsletter(models.Model):
+
+    subject = models.CharField(null=True, max_length=254)
+    content = models.TextField(null=True, max_length=254)
